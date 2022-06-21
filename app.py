104c4ae98c8c8f81aa23a04c48d3b6343044638e
Vexoben <2493583737@qq.com>
2022-06-21 14:15:09 +0800

    最后提交版，暂无继续更新的打算

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ courier = Courier('./code')
 @app.route('/', methods = ['POST','GET'])
 def hello():
     courier.add_user('you_know_who', 'you_know_who', 'you_know_what', '大摆锤', 'vegetable@sjtu.edu.cn', 10)
+    user_info = request.form.to_dict()
+    if ('current_user' in user_info.keys() and user_info['current_user'] != ''):
+        return render_template('./user.html', current_user = user_info['current_user'])
     if (request.form.get('username') is not None):
         us = request.form.get('username')
         ps = request.form.get('password')
@@ -68,6 +71,7 @@ def personal_profile():
 @app.route('/modify_user', methods = ['POST', 'GET'])
 def modify_user():
     user_info = request.form.to_dict()
+    print(user_info)
     if (not('current_user' in user_info.keys() and user_info['current_user'] != '')):
         return render_template('./register.html')
     if ('username' in user_info.keys() and user_info['username'] != ''):
@@ -75,8 +79,8 @@ def modify_user():
         username = user_info['username']
         cu_info = courier.query_profile(current_user, current_user)
         us_info = courier.query_profile(current_user, username)
-        if (us_info[0] == '-1' or int(cu_info[3]) < int(user_info['privilege'])):
-            return render_template('./modify_user.html', fail_type = 1)
+        if ('privilege' in user_info.keys() and user_info['privilege'] != '' and (us_info[0] == '-1' or int(cu_info[3]) < int(user_info['privilege']))):
+            return render_template('./modify_user.html', fail_type = 1, current_user = current_user)
         else:
             s = '[0] modify_profile -c ' + current_user + ' -u ' + username
             if ('name' in user_info.keys() and user_info['name'] != ''):
@@ -93,7 +97,7 @@ def modify_user():
             else :
                 return render_template('./modify_user.html', fail_type = 0, current_user = current_user)
     else:
-        return render_template('./modify_user.html', current_user = '', fail_type = 2)
+        return render_template('./modify_user.html', current_user = user_info['current_user'], fail_type = 2)
 
 @app.route('/add_user', methods = ['POST', 'GET'])
 def add_user():
@@ -108,34 +112,64 @@ def add_user():
         if ('privilege' in user_info.keys() and user_info['privilege'] != ''):
             s = s + ' -g ' + user_info['privilege']
         else:
-            return render_template('add_user.html', fail_type = 4)
+            return render_template('add_user.html', fail_type = 4, current_user = current_user)
         cu_info = courier.query_profile(current_user, current_user)
         us_info = courier.query_profile(current_user, username)
         if (us_info[0] != '-1' or int(cu_info[3]) <= int(user_info['privilege'])):
-            return render_template('add_user.html', fail_type = 3)
+            return render_template('add_user.html', fail_type = 3, current_user = current_user)
         else:
             if ('name' in user_info.keys() and user_info['name'] != ''):
                 s = s + ' -n ' + user_info['name']
             else:
-                return render_template('add_user.html', fail_type = 4)
+                return render_template('add_user.html', fail_type = 4, current_user = current_user)
             if ('mailAddr' in user_info.keys() and user_info['mailAddr'] != ''):
                 s = s + ' -m ' + user_info['mailAddr']
             else:
-                return render_template('add_user.html', fail_type = 4)
+                return render_template('add_user.html', fail_type = 4, current_user = current_user)
             if ('password' in user_info.keys() and user_info['password'] != ''):
                 s = s + ' -p ' + user_info['password']
             else:
-                return render_template('add_user.html', fail_type = 4)
+                return render_template('add_user.html', fail_type = 4, current_user = current_user)
             res = courier.add_user2(s)
             return render_template('./add_user.html', fail_type = 1, current_user = current_user)
-    elif ('current_user' in user_info.keys() and user_info['current_user'] != ''):
-        return render_template('./add_user.html', current_user = user_info['current_user'], fail_type = 0)
     else:
-        return render_template('./add_user.html', current_user = '', fail_type = 0)
+        return render_template('./add_user.html', current_user = user_info['current_user'], fail_type = 4)
 
-@app.route('/user', methods = ['POST', 'GET'])
-def user():
-    return render_template("./user.html")      
+@app.route('/train', methods = ['POST', 'GET'])
+def train():
+    return render_template("./train.html")
+
+@app.route('/add_train', methods = ['POST', 'GET'])
+def add_train():
+    return render_template("./add_train.html")      
+
+@app.route('/delete_train', methods = ['POST', 'GET'])
+def delete_train():
+    return render_template("./delete_train.html")      
+
+@app.route('/release_train', methods = ['POST', 'GET'])
+def release_train():
+    return render_template("./release_train.html")      
+
+@app.route('/query_train', methods = ['POST', 'GET'])
+def query_train():
+    return render_template("./query_train.html")
+
+@app.route('/ticket', methods = ['POST', 'GET'])
+def ticket():
+    return render_template("./ticket.html")      
+
+@app.route('/buy_ticket', methods = ['POST', 'GET'])
+def buy_ticket():
+    return render_template("./buy_ticket.html")
+
+@app.route('/query_ticket', methods = ['POST', 'GET'])
+def query_ticket():
+    return render_template("./query_ticket.html")
+
+@app.route('/refund_ticket', methods = ['POST', 'GET'])
+def refund_ticket():
+    return render_template("./refund_ticket.html")
 
 
 if __name__ == '__main__':
